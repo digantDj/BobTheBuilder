8bf030cb6b0b725b7ecc520a7cd0802a56bda994
digantDj <digant.jagtap@listertechnologies.com>
2015-04-12 12:30:28 +0530

    Basic Flow implementation with scores in backnd

--- src/com/example/bobthebuilder/MainActivity.java
+++ src/com/example/bobthebuilder/MainActivity.java
@@ -4,14 +4,20 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View; 
@@ -21,20 +27,28 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-
 public class MainActivity extends Activity  implements OnClickListener {
 
 	private MalibuCountDownTimer countDownTimer;
 	private boolean timerHasStarted = false;
 	private Button startB;
 	private TextView text;
+	
+	private NotificationManager mNotificationManager;
+	private int notificationID = 100;
+	private int numMessages = 0;
 
 	private final long startTime = 1000*60*30;
 	private final long interval = 1000;
 	
-    private BroadcastReceiver mReceiver;
-     
+	
+	SharedPreferences sharedpreferences;
+	public static final String MyPREFERENCES = "bobPrefs" ;
+	public static final String BuiltScore = "builtKey"; 
+	public static final String UnBuiltScore = "unBuiltKey";
+	private String bobBuildScore = "0";
+	private String bobUnBuildScore = "0";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,40 +66,49 @@ public class MainActivity extends Activity  implements OnClickListener {
 
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_USER_PRESENT);
-
-        mReceiver = new ScreenReceiver();
-        registerReceiver(mReceiver, filter);
+        
+        //Retrieve Stored Data
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(BuiltScore))
+        {
+        	bobBuildScore = sharedpreferences.getString(BuiltScore, "");
+        }
+        if (sharedpreferences.contains(UnBuiltScore))
+        {
+        	bobUnBuildScore = sharedpreferences.getString(UnBuiltScore, "");
+        }
     }
     
     @Override
     public void onPause() {
         super.onPause();
-        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-
-        boolean screenOn;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            //screenOn = pm.isInteractive();
-        	screenOn = pm.isScreenOn();
-        } else {
-            screenOn = pm.isScreenOn();
-        }
-
-        if (screenOn) {
-            // Screen is still on, so do your thing here
-        	Log.v("$$$$$$", "In Method: onPause()");
-        }
-        
+       	Log.v("$$$$$$", "In Method: onPause()");
+    
     }
     
     @Override
     public void onStop() {
+    	PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+        boolean screenOn;
+        screenOn = pm.isScreenOn();
     	super.onStop();
+    	if (screenOn) {
     	countDownTimer.cancel();
 		timerHasStarted = false;
-		text.setText("30:00");
-		startB.setText("Build");
-    	tToast("onStop.");
+		text.setText(getText(R.string.timerInitVal));
+		startB.setText(getText(R.string.startButtonLabel));
+		// Store values between instances here
+	       	Editor editor = sharedpreferences.edit();
+	       bobUnBuildScore = Integer.toString((Integer.parseInt(bobUnBuildScore) + 1));
+	        editor.putString(UnBuiltScore, bobUnBuildScore);
+	        Log.v("$$$$$$", "unBuiltScore Value:"+bobUnBuildScore);
+        // Commit to storage
+	        editor.commit();
+    	//tToast("onStop.");
+		displayNotification();
     	Log.v("$$$$$$", "In Method: onStop()");
+    	}
     }
     
     @Override
@@ -93,21 +116,27 @@ public class MainActivity extends Activity  implements OnClickListener {
         super.onResume();  // Always call the superclass method first
         //tToast("onResume.");
         Log.v("$$$$$$", "In Method: onResume()");
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(BuiltScore))
+        {
+           tToast(sharedpreferences.getString(BuiltScore, ""));
+        }
+        if (sharedpreferences.contains(UnBuiltScore))
+        {
+           tToast(sharedpreferences.getString(UnBuiltScore, ""));
+        }
     }
-    
+        
     
     @Override
 	public void onClick(View v)	{
 		if (!timerHasStarted) {
 			countDownTimer.start();
 			timerHasStarted = true;
-			startB.setText("Kill Bob");
+			startB.setText(getText(R.string.duringBuildButtonLabel));
 		}
 		else {
-			countDownTimer.cancel();
-			timerHasStarted = false;
-			text.setText("30:00");
-			startB.setText("Build");
+			alertMessage();
 		}
 	}
     	
@@ -120,7 +149,14 @@ public class MainActivity extends Activity  implements OnClickListener {
 
  		@Override
  		public void onFinish(){
- 						text.setText("Time's up!");
+ 	        // Store values between instances here
+ 	       	Editor editor = sharedpreferences.edit();
+ 	       bobBuildScore = Integer.toString((Integer.parseInt(bobBuildScore) + 1));
+ 	        editor.putString(BuiltScore, bobBuildScore);
+ 	       Log.v("$$$$$$", "BuiltScore Value:"+bobBuildScore);     
+ 	        // Commit to storage
+ 	        editor.commit();
+ 			text.setText(getText(R.string.timeUpMessage));
  		}
 
  		@Override
@@ -135,6 +171,38 @@ public class MainActivity extends Activity  implements OnClickListener {
 			int minutes = (int) ((milliseconds / 1000) / 60);
  			String finalString = f.format(minutes) + ":" + f.format(seconds);
 			return finalString;
+ 	}
+ 	
+ 	public void alertMessage() {
+         DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                      switch (which) {
+                      case DialogInterface.BUTTON_POSITIVE:
+                             // Yes button clicked
+                    	  		countDownTimer.cancel();
+              					timerHasStarted = false;
+              					text.setText(getText(R.string.timerInitVal));
+              					startB.setText(getText(R.string.startButtonLabel));
+              				// Store values between instances here
+              		 	       	Editor editor = sharedpreferences.edit();
+              		 	       	bobUnBuildScore = Integer.toString(Integer.parseInt(bobUnBuildScore)+1);
+              		 	        editor.putString(UnBuiltScore, bobUnBuildScore);
+              		 	        Log.v("$$$$$$", "UnBuiltScore Value:"+bobUnBuildScore);
+           		 	        // Commit to storage
+              		 	        editor.commit();            					
+                             break;
+                      case DialogInterface.BUTTON_NEGATIVE:
+                             // No button clicked
+                             // do nothing                             
+                             break;
+                      }
+                }
+         	};
+
+         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+         builder.setMessage("You sure want to kill the cute little Bob ?")
+                      .setPositiveButton("Yes, destroy the building also!!", dialogClickListener)
+                      .setNegativeButton("No, can think again!!", dialogClickListener).show();
  	}
  		
     @Override
@@ -161,13 +229,7 @@ public class MainActivity extends Activity  implements OnClickListener {
     {
           super.onDestroy();
           Log.v("$$$$$$", "In Method: onDestroy()");
-         
-          if (mReceiver != null)
-          {
-                unregisterReceiver(mReceiver);
-                mReceiver = null;
-          }          
-       
+            
     }
     
     @Override
@@ -191,4 +253,41 @@ public class MainActivity extends Activity  implements OnClickListener {
         Toast toast = Toast.makeText(context, s, duration);
         toast.show();
     }
+    
+    protected void displayNotification() {
+        Log.v("Start", "notification");
+
+        /* Invoking the default notification service */
+        NotificationCompat.Builder  mBuilder =  new NotificationCompat.Builder(this);	
+
+        mBuilder.setContentTitle("New Message");
+        mBuilder.setContentText("You've received new message.");
+        mBuilder.setTicker("New Message Alert!");
+        mBuilder.setSmallIcon(R.drawable.woman);
+
+        /* Increase notification number every time a new notification arrives */
+        mBuilder.setNumber(++numMessages);
+        
+        /* Creates an explicit intent for an Activity in your app */
+        Intent resultIntent = new Intent(this, NotificationView.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(NotificationView.class);
+
+        /* Adds the Intent that starts the Activity to the top of the stack */
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+           stackBuilder.getPendingIntent(
+              0,
+              PendingIntent.FLAG_UPDATE_CURRENT
+           );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        mNotificationManager =
+        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        /* notificationID allows you to update the notification later on. */
+        mNotificationManager.notify(notificationID, mBuilder.build());
+     }
 }
